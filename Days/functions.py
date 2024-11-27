bd556ae5909c532bf4d4c09b06b662b5756bcbbc
Thomas <hexagamesr@gmail.com>
2024-11-26 23:47:15 -0500

    ajouter un nouveau todo

--- Days/functions.py
+++ Days/functions.py
@@ -1,14 +1,23 @@
 FILEPATH = "todos.txt"
-
-
+import streamlit as st
+"""Reecrit la liste a faire dans le document .txt ."""
 def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, "w") as file :
         file.writelines(todos_arg)
 
+"""Receuille la liste du document .txt a l'ouverture du programme."""
 def get_todos(filepath=FILEPATH):
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-if __name__ == "__main__" :
-    print("Bonjour!")
+"""Ajoute un point dans la version web-app"""
+#def add_todo():
+   # todo = st.session_state["nouveau_todo"]
+   # todos.append(todo)
+   # write_todos(todos)
+
+
+"""Complete un point dans la version web app """
+
+
--- Days/web.py
+++ Days/web.py
@@ -3,13 +3,26 @@ from functions import get_todos, write_todos
 
 todos = get_todos()
 
+def add_todo():
+    todo = st.session_state["nouveau_todo"] + "\n"
+    todos.append(todo)
+    write_todos(todos)
+
 st.title("Mon Application De Liste ")
 st.write("Ceci est un PP de Thomas Gaudreault")
 
 for todo in todos:
-    st.checkbox(todo)
+   st.checkbox(todo)
 
-st.text_input(label = "" , placeholder = "Ajouter un point: ")
+st.text_input(label = "" , placeholder = "Ajouter un point: ",
+              on_change = add_todo , key = "nouveau_todo")
+
+
+
+
+
+
+
 
 
 
